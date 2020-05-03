6dcda203760eb7bf0cd045ddc923b16fddb99b55
logan Gray <ld.gray@setonhill.edu>
2020-05-03 16:18:59 -0400

    Initial Commit

---
+++ Gray HW #8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gray HW #8
+//
+//  Created by logan gray on 5/3/20.
+//  Copyright © 2020 logan gray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Gray HW #8Tests/Gray_HW__8Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Gray_HW__8Tests.swift
+//  Gray HW #8Tests
+//
+//  Created by logan gray on 5/3/20.
+//  Copyright © 2020 logan gray. All rights reserved.
+//
+
+import XCTest
+@testable import Gray_HW__8
+
+class Gray_HW__8Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
